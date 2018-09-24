7d874e2c2baedf6b5547d0bfbdc15253cd7d2e78
haritha-j <harithajayasinghe@gmail.com>
2018-09-24 23:12:19 +0530

    added retraining code

---
+++ node 1/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const net = await posenet.load(0.5);
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.end ('Hello');
+});
+
+server.listen(port, hostname, () => {
+    console.log("Server started on port " + port);
+}); 
